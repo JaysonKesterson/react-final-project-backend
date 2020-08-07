111020b9c9bbffaa229cf3900f934f061b3b7df4
jayson_kesterson <your_email@example.com>
2020-08-07 15:08:58 -0400

    adding get current user route to the sessions corresponding action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   post "api/v1/login", to: "api/v1/sessions#create"
+  delete "api/v1/logout", to: "api/v1/sessions#destroy"
   get "api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users 
       resources :stores
       resources :items
     end
