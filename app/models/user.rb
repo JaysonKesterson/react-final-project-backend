b27fd9755e9ea15015ed60c03c39dcc827c56c53
jayson_kesterson <your_email@example.com>
2020-08-09 10:56:42 -0400

    adding validations to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_one :store
     has_many :items, through: :store
+
+    validates :name, :email, presence: true
+    validates :email, uniqueness: true
 end
