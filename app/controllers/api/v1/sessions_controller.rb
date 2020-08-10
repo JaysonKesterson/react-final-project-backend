3e7bdaddf00797a299fb6cf77a3dbf4eda2f3184
jayson_kesterson <your_email@example.com>
2020-08-10 09:22:07 -0400

    modifying how a user is created in sessions

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -26,7 +26,7 @@ class Api::V1::SessionsController < ApplicationController
         session.clear
         render json: {
             notice: "Logout Successful"
-        }, status: ok
+        }, status: :ok
     end
 
 end
