9c4e47d1c9c9ed3028d520ecf374b397380fcd3e
jayson_kesterson <your_email@example.com>
2020-08-16 10:08:27 -0400

    experimenting with has_many, deciding on has_one

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_one :store
-    has_many :items, through: :store
+    has_many :items, through: :stores
 
     validates :name, :email, presence: true
     validates :email, uniqueness: true
