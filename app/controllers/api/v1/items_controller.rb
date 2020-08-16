1baf5df37d33b7169617d19f955e68368ab82030
jayson_kesterson <your_email@example.com>
2020-08-16 10:03:10 -0400

    updating items controller to handle front end crud

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,57 @@
 class Api::V1::ItemsController < ApplicationController
-    def index
-        @items = Item.all
-    
-        render json: @items
+
+  before_action :set_item, only: [:show, :update, :destroy]
+
+  def index
+      @items = Item.all
+  
+      items_json = ItemSerializer.new(@items).serialized_json
+      render json: items_json
+    end
+
+    def create
+      
+      @item = Item.new(item_params)
+      serialized_item = ItemSerializer.new(@item)
+      if @item.save
+        render json: serialized_item, status: :created
+      else
+        resp = {
+          error: @item.errors.full_messages.to_sentence
+        }
+        render json: resp, status: :unprocessable_entity
       end
+    end
+
+    def show
+      item_json = ItemSerializer.new(@item)
+      render json: item_json
+    end
+
+    def update
+      if @item.update(item_params)
+        render json: ItemSerializer.new(@item)
+      else
+        render json: @item.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+
+      if @item.destroy
+        render json: { data: "Item successfully destroyed" }, status: :ok
+      else
+        render json: @item.errors, status: :unprocessable_entity
+      end
+    end
+
+    def item_params
+      params.require(:item).permit(:store_id, :name, :price, :image_url, :description, :condition)
+    end
+
+    private
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
 end
