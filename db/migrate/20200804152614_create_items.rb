eed0a39bfab7c3cd1cd53f36733c0586e53c01d5
jayson_kesterson <your_email@example.com>
2020-08-16 10:03:30 -0400

    adding image_url to items

--- db/migrate/20200804152614_create_items.rb
+++ db/migrate/20200804152614_create_items.rb
@@ -1,8 +1,9 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string :image_url
       t.integer :store_id
-      t.string :price
+      t.decimal :price
       t.string :name
       t.string :description
       t.integer :condition
